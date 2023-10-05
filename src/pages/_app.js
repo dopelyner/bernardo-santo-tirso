b301cbcd2e112afc5729096d4db09615626f80ac
dopelyner <dalyner@protonmail.com>
2023-10-05 15:29:49 +0000

    removed unnecessary content

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import '@/styles/globals.css'
 import { AnimatePresence } from 'framer-motion'
-import { Montserrat, Roboto_Flex } from "next/font/google"
+import { Montserrat } from "next/font/google"
 import { useRouter } from 'next/router'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
@@ -9,11 +9,6 @@ import Footer from '@/components/Footer'
 const montserrat = Montserrat({
   subsets: ["latin"],
   variable: "--font-mont"
-})
-
-const roboto = Roboto_Flex({
-  subsets: ["latin"],
-  variable: "--font-roboto"
 })
 
 export default function App({ Component, pageProps }) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,6 @@ module.exports = {
     extend: {
       fontFamily: {
         mont: ['var(--font-mont)', ...fontFamily.sans],
-        roboto: ['var(--font-roboto)', ...fontFamily.sans],
       },
       colors: {
         dark: "#1b1b1b",
