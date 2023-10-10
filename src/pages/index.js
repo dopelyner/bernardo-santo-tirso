e341bdd622b4c72da8399c6f6be62d7649c5cbee
dopelyner <dalyner@protonmail.com>
2023-10-10 17:11:20 +0000

    home: added downarrow & fixed title responsive size

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
+import ReactPlayer from 'react-player'
 import Layout from '@/components/Layout'
 import TransitionEffect from '@/components/TransitionEffect'
-import ReactPlayer from 'react-player'
-import { presentation } from '@/constants'
 import AnimatedText from '@/components/AnimatedText'
 import { Stick } from 'next/font/google'
+import { DownArrow } from '@/components/Icons'
+import { presentation } from '@/constants'
 
 const font = Stick({
   subsets: ["latin"],
@@ -22,7 +23,7 @@ export default function Home() {
         <meta name="Bernardo Santo Tirso" content="Página Oficial" />
       </Head>
 
-      {/* <TransitionEffect /> */}
+      <TransitionEffect />
 
       <main className='w-full min-h-screen flex items-center text-dark dark:text-light'>
         <Layout className='pt-0 p-16 sm:pt-8'>
@@ -30,16 +31,21 @@ export default function Home() {
           <AnimatedText
             style={font.style}
             text="Bernardo Santo Tirso"
-            className='mb-16 text-[200px] text-center lg:p-8 xs:text-8xl xs:p-8'
+            className='mb-60 text-[200px] text-center lg:p-8 lg:text-9xl sm:text-6xl xs:text-6xl xs:p-8 xs:mb-40'
           />
 
-          {/* <div className='w-full flex items-center justify-center md:w-[600px] sm:w-auto '>
-              <ReactPlayer
-                url={videoURL}
-                width={1280}
-                height={720}
-              />
-            </div> */}
+          <div className='w-full flex flex-col items-center justify-center '>
+            <h2>check this out</h2>
+            <DownArrow />
+          </div>
+
+          <div className='w-full mt-4 flex items-center justify-center md:w-[600px] sm:w-auto '>
+            <ReactPlayer
+              url={videoURL}
+              width={1280}
+              height={720}
+            />
+          </div>
 
         </Layout>
       </main>
