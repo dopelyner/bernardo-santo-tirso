d54fe58ad5551393072c3c3d8d33ab8c86f5e2e2
dopelyner <dalyner@protonmail.com>
2023-10-21 16:35:16 +0000

    project: added subtitle to project item

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,6 +9,34 @@ import { quotes, videoLinks } from '@/constants'
 
 const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
+const ProjectItem = ({ title, subTitle, videoURL }) => {
+    return (
+        <div className='flex flex-col mb-24'>
+            <AnimatedText
+                text={title}
+                className='mt-0 italic text-5xl xs:text-4xl text-center'
+            />
+            <h3
+                className='mt-0 text-2xl xs:text-xl text-center mb-2'>
+                {subTitle}
+            </h3>
+            <div
+                id="video2"
+                className='w-[1080px] h-[600px] flex flex-col items-center justify-center xl:w-[720px] xl:h-[500px]
+                    lg:w-[600px] lg:h-[400px] sm:w-[400px] sm:h-[200px] sm:self-center xs:w-[280px] xs:h-[200px] my-0 mx-2'
+            >
+                <ReactPlayer
+                    url={videoURL}
+                    width="100%"
+                    height="100%"
+                    controls={true}
+                    className="w-full sm:w-4/5 lg:w-3/5 xl:w-2/5"
+                />
+            </div>
+        </div>
+    )
+}
+
 const Projects = () => {
     return (
         <>
@@ -19,7 +47,7 @@ const Projects = () => {
 
             {/* <TransitionEffect /> */}
 
-            <main className='w-full mb-16 flex flex-col items-center min-h-screen justify-center overflow-hidden dark:text-light'>
+            <main className='w-full mb-16 flex flex-col items-center min-h-screen justify-center overflow-hidden text-dark dark:text-light'>
                 <Layout className='pt-16'>
 
                     <div className='w-full flex flex-col lg:p-8 xs:text-6xl xs:p-8'>
@@ -38,32 +66,18 @@ const Projects = () => {
                         <h2 className='mb-32 text-2xl text-end mr-32 xs:m-0 xs:mb-32'>{quotes.author} 2/3</h2>
                     </div>
 
-
                     <div className='flex flex-col items-center justify-center lg:mt-0'>
                         <div className='flex flex-col mb-16'>
                             <AnimatedText
                                 text="Ator ( Teatro & Cinema)"
                                 className='w-full mb-32 xs:text-5xl text-center'
                             />
-                            <div className='flex flex-col'>
-                                <AnimatedText
-                                    text="Ride"
-                                    className='mt-0 italic text-5xl xs:text-4xl text-center'
-                                />
-                                <div
-                                    id="video2"
-                                    className='w-[1080px] h-[720px] flex flex-col items-center justify-center xl:w-[720px] lg:w-[720px] 
-                                                md:w-[600px] sm:w-[400px] xs:w-[300px]'
-                                >
-                                    <ReactPlayer
-                                        url={videoLinks.URL_2}
-                                        width="100%"
-                                        height="100%"
-                                        controls={true}
-                                        className="w-full sm:w-4/5 lg:w-3/5 xl:w-2/5"
-                                    />
-                                </div>
-                            </div>
+
+                            <ProjectItem
+                                title="Ride"
+                                subTitle="Filme de Animação realizado por Paul Bush em 2017"
+                                videoURL={videoLinks.URL_2}
+                            />
                         </div>
 
                         <SeparatorBar />
