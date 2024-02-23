f618a1be464543644a9e67b267fc5d5f7bf1b5aa
dopelyner <dalyner@protonmail.com>
2024-02-23 12:48:19 -0100

    font optimization

--- src/components/TitleStickText.js
+++ src/components/TitleStickText.js
@@ -1,14 +1,10 @@
-import React from 'react'
-import localFont from 'next/font/local'
 import AnimatedText from './AnimatedText';
 
-const StickFont = localFont({ src: '../../public/fonts/Stick-Regular.ttf', display: 'swap' })
-
-const TitleStickText = ({ title, className = "" }) => {
+const TitleStickText = ({ font, title, className = "" }) => {
     return (
         <AnimatedText
             text={title}
-            className={`${StickFont.className} ${className} font-stick text-[220px] 
+            className={`${font} ${className} font-stick text-[220px] 
             text-center xl:text-[150px] 2xl:text-[200px] sm:text-[60px]`}
         />
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import Layout from '@/components/Layout'
 import TitleStickText from '@/components/TitleStickText'
 import TransitionEffect from '@/components/TransitionEffect'
+import localFont from 'next/font/local'
 import Head from 'next/head'
+
+const StickFont = localFont({ src: '../../public/fonts/Stick-Regular.ttf', display: 'swap' })
 
 export default function Home() {
 
@@ -17,7 +20,7 @@ export default function Home() {
       <main className='w-full min-h-96 flex  justify-center text-dark dark:text-light sm:justify-center'>
         <Layout className='pt-0'>
 
-          <TitleStickText title="Bernardo Santo Tirso" className='mt-16' />
+          <TitleStickText title="Bernardo Santo Tirso" className='mt-16' font={StickFont.className}/>
 
         </Layout>
       </main>
