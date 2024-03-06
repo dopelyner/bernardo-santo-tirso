5a75e4d2f9c12cf972fca962a0d696eb7463b90b
dopelyner <dalyner@protonmail.com>
2024-03-06 13:21:00 -0100

    fix: biography big screen layout

--- src/pages/biography.js
+++ src/pages/biography.js
@@ -14,7 +14,7 @@ const Biography = () => {
                 <meta name="Bernardo Santo Tirso" content='Biografia...' />
             </Head>
 
-            <main className='w-full mb-16 flex flex-col items-center min-h-screen justify-center overflow-hidden dark:text-light'>
+            <main className='w-full flex flex-col items-center min-h-screen justify-center overflow-hidden dark:text-light'>
                 <Layout className='pt-16'>
 
                     <div className='w-full mb-24 flex flex-col lg:p-8 xs:text-6xl xs:p-8'>
@@ -39,51 +39,56 @@ const Biography = () => {
 
                         <AnimatedText
                             text="Biografia"
-                            className='w-1/2 mt-16 text-center xs:mb-6 xs:text-7xl lg:text-7xl '
+                            className='w-1/2 mb-16 text-center xs:mb-6 xs:text-7xl lg:text-7xl '
                         />
 
-                        <div className='flex flex-row gap-x-10  justify-center items-center xl:flex-col'>
-                            <div className='flex flex-col gap-4 justify-center items-center'>
-                                <Image
-                                    src={imageLinks.PIC_BIOGRAPHY01}
-                                    width={400}
-                                    height={400}
-                                    alt='Bernardo Sto Tirso 1'
-                                    className='rounded-xl border-2 border-black dark:border-light'
-                                />
-                                <h3 className='text-lg'>Fotografia de João Pádua</h3>
-                            </div>
-                            <div className='mt-6 text-xl xs:text-2xl'>
-                                {biographyText.paragraph01.map((paragraph, index) => (
-                                    <li
-                                        key={index}
-                                        className='list-none my-6'
-                                    >
-                                        {paragraph}
-                                    </li>
-                                ))}
-                            </div>
-                            <div className='flex flex-col mt-10 gap-4 justify-center items-center'>
-                                <Image
-                                    src={imageLinks.PIC_BIOGRAPHY02}
-                                    width={400}
-                                    height="auto"
-                                    alt='Bernardo Sto Tirso 2'
-                                    className='rounded-xl border-2 border-black dark:border-light'
-                                />
-                                <h3 className='text-lg'>Fotografia de João Pádua</h3>
-                            </div>
-                            <div className='mt-6 text-xl xs:text-2xl'>
-                                {biographyText.paragraph02.map((paragraph, index) => (
-                                    <li
-                                        key={index}
-                                        className='list-none my-6'
-                                    >
-                                        {paragraph}
-                                    </li>
-                                ))}
+                        <div className='flex flex-col justify-center items-center'>
+
+                            <div className='flex flex-row items-center xl:flex-col gap-x-10 '>
+                                <div className='flex flex-col gap-4 justify-center items-center'>
+                                    <Image
+                                        src={imageLinks.PIC_BIOGRAPHY01}
+                                        width={400}
+                                        height={400}
+                                        alt='Bernardo Sto Tirso 1'
+                                        className='rounded-xl border-2 border-black dark:border-light'
+                                    />
+                                    <h3 className='text-lg'>Fotografia de João Pádua</h3>
+                                </div>
+                                <div className='mt-6 text-xl xs:text-2xl'>
+                                    {biographyText.paragraph01.map((paragraph, index) => (
+                                        <li
+                                            key={index}
+                                            className='list-none my-6'
+                                        >
+                                            {paragraph}
+                                        </li>
+                                    ))}
+                                </div>
                             </div>
 
+                            <div className='flex flex-row-reverse items-center xl:flex-col gap-x-10 '>
+                                <div className='flex flex-col gap-4 justify-center items-center'>
+                                    <Image
+                                        src={imageLinks.PIC_BIOGRAPHY02}
+                                        width={400}
+                                        height="auto"
+                                        alt='Bernardo Sto Tirso 2'
+                                        className='rounded-xl border-2 border-black dark:border-light'
+                                    />
+                                    <h3 className='text-lg'>Fotografia de João Pádua</h3>
+                                </div>
+                                <div className='mt-6 text-xl xs:text-2xl'>
+                                    {biographyText.paragraph02.map((paragraph, index) => (
+                                        <li
+                                            key={index}
+                                            className='list-none my-6'
+                                        >
+                                            {paragraph}
+                                        </li>
+                                    ))}
+                                </div>
+                            </div>
                         </div>
                     </div>
 
