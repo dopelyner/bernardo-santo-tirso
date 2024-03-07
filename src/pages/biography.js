a7e59dc172377a8a74505c1d1e491410afb82d29
dopelyner <dalyner@protonmail.com>
2024-03-07 18:26:27 -0100

    fix: divided image with text

--- src/pages/biography.js
+++ src/pages/biography.js
@@ -42,54 +42,38 @@ const Biography = () => {
                             className='w-1/2 mb-16 text-center xs:mb-6 xs:text-7xl lg:text-7xl '
                         />
 
-                        <div className='flex flex-col justify-center items-center'>
-
-                            <div className='flex flex-row items-center xl:flex-col gap-x-10 '>
-                                <div className='flex flex-col gap-4 justify-center items-center'>
-                                    <Image
-                                        src={imageLinks.PIC_BIOGRAPHY01}
-                                        width={400}
-                                        height={400}
-                                        alt='Bernardo Sto Tirso 1'
-                                        className='rounded-xl border-2 border-black dark:border-light'
-                                    />
-                                    <h3 className='text-lg'>Fotografia de João Pádua</h3>
-                                </div>
-                                <div className='mt-6 text-xl xs:text-2xl'>
-                                    {biographyText.paragraph01.map((paragraph, index) => (
-                                        <li
-                                            key={index}
-                                            className='list-none my-6'
-                                        >
-                                            {paragraph}
-                                        </li>
-                                    ))}
-                                </div>
+                        <div className='flex flex-row xl:flex-col justify-center items-center gap-x-6'>
+                            <div className='relative w-fit h-fit flex flex-col'>
+                                <Image
+                                    src={imageLinks.PIC_BIOGRAPHY01}
+                                    width="100%"
+                                    height="100%"
+                                    alt='Bernardo Sto Tirso 1'
+                                />
+                                <h3 className='absolute bottom-0 w-full text-light 
+                                text-center bg-transparent/40 text-xl p-4 hover:bg-transparent/60'>Fotografia de João Pádua</h3>
                             </div>
+                            <div className='w-screen flex flex-col max-w-prose text-3xl xs:text-2xl text-justify'>
+                                {biographyText.paragraph01.map((paragraph, index) => (
+                                    <li
+                                        key={index}
+                                        className='list-none my-6'
+                                    >
+                                        {paragraph}
+                                    </li>
+                                ))}
 
-                            <div className='flex flex-row-reverse items-center xl:flex-col gap-x-10 '>
-                                <div className='flex flex-col gap-4 justify-center items-center'>
-                                    <Image
-                                        src={imageLinks.PIC_BIOGRAPHY02}
-                                        width={400}
-                                        height="auto"
-                                        alt='Bernardo Sto Tirso 2'
-                                        className='rounded-xl border-2 border-black dark:border-light'
-                                    />
-                                    <h3 className='text-lg'>Fotografia de João Pádua</h3>
-                                </div>
-                                <div className='mt-6 text-xl xs:text-2xl'>
-                                    {biographyText.paragraph02.map((paragraph, index) => (
-                                        <li
-                                            key={index}
-                                            className='list-none my-6'
-                                        >
-                                            {paragraph}
-                                        </li>
-                                    ))}
-                                </div>
+                                {biographyText.paragraph02.map((paragraph, index) => (
+                                    <li
+                                        key={index}
+                                        className='list-none my-6'
+                                    >
+                                        {paragraph}
+                                    </li>
+                                ))}
                             </div>
                         </div>
+
                     </div>
 
                 </Layout>
