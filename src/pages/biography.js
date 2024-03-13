1c5a3dcdc124084b500080ed5fbde78b503c5369
dopelyner <dalyner@protonmail.com>
2024-03-13 13:30:19 -0100

    fix: layout for small devices

--- src/pages/biography.js
+++ src/pages/biography.js
@@ -40,47 +40,53 @@ const Biography = () => {
                 </Layout>
 
                 <section className='w-full min-h-full flex flex-row xl:flex-col justify-center items-start gap-x-6 bg-dark dark:bg-primary'>
-                    <div className='relative w-1/2 flex flex-col self-center'>
+                    <div className='relative w-1/2 xl:w-full sm:w-full flex flex-col self-center'>
                         <Image
                             src={imageLinks.PIC_BIOGRAPHY01}
                             width="100%"
                             height="100%"
                             alt='Bernardo Sto Tirso 1'
-                            className='max-h-[1200px]'
+                            className='w-max h-max xl:w-max'
                         />
                         <h3 className='absolute -bottom-0 bg-dark/60 w-full text-light italic 
-                                text-center text-xl p-6'>Fotografia de João Pádua</h3>
+                                text-center text-xl p-6 xl:p-2 sm:p-2 sm:text-sm'>Fotografia de João Pádua</h3>
                     </div>
 
                     <div className='flex flex-col w-full'>
                         <AnimatedText text="A Grande Caminhada" className='text-center p-16 text-light' />
 
-                        <Accordion>
-                            {biographyAccordion.map((item, index) => (
+                        <div className='flex justify-center items-center'>
+                            <Accordion>
+                                {biographyAccordion.map((item, index) => (
 
-                                <AccordionItem
-                                    key={index}
-                                    aria-label={item.title}
-                                    title={item.title}
-                                    className='text-6xl px-8 py-4 text-justify flex flex-col border-b-blue
-                                     hover:text-blue border-b-2 text-light hover:-translate-y-1'
-                                    indicator={({ isOpen }) => (isOpen ? <MinusCircle /> : <PlusCircle />)}
-                                >
-                                    <p className='text-2xl text-light pb-2'>{item.content[0]}</p>
-                                    <p className='text-2xl text-light py-2'>{item.content[1]}</p>
-                                    <p className='text-2xl text-light py-2'>{item.content[2]}</p>
-                                    <p className='text-2xl text-light pt-2'>{item.content[3]}</p>
-                                </AccordionItem>
-                            ))}
-                        </Accordion>
+                                    <AccordionItem
+                                        key={index}
+                                        aria-label={item.title}
+                                        title={item.title}
+                                        className='text-6xl px-8 py-4 text-justify flex flex-col border-b-blue
+                                    hover:text-blue border-b-2 text-light hover:-translate-y-1'
+                                        indicator={({ isOpen }) => (isOpen ? <MinusCircle /> : <PlusCircle />)}
+                                    >
+                                        <p className='text-2xl text-light pb-2'>{item.content[0]}</p>
+                                        <p className='text-2xl text-light py-2'>{item.content[1]}</p>
+                                        <p className='text-2xl text-light py-2'>{item.content[2]}</p>
+                                        <p className='text-2xl text-light pt-2'>{item.content[3]}</p>
+                                    </AccordionItem>
+                                ))}
+                            </Accordion>
+                        </div>
                     </div>
                 </section>
 
-                <div className='flex flex-row xl:flex-col my-24 gap-20'>
-                    <CustomLink href="/cinema" title="Cinema" className='w-full text-8xl xs:text-5xl text-center' />
-                    <CustomLink href="/theater" title="Teatro" className='w-full text-8xl xs:text-5xl text-center' />
-                    <CustomLink href="/light" title="Luz" className='w-full text-8xl xs:text-5xl text-center' />
-                    <CustomLink href="/staging" title="Encenação" className='w-full text-8xl xs:text-5xl text-center' />
+                <div className='flex flex-col items-center justify-center my-20'>
+                    <h3 className='text-3xl italic'>Projectos</h3>
+
+                    <div className='flex flex-row xl:flex-col my-12  gap-20'>
+                        <CustomLink href="/cinema" title="Cinema" className='w-full text-8xl xs:text-5xl text-center' />
+                        <CustomLink href="/theater" title="Teatro" className='w-full text-8xl xs:text-5xl text-center' />
+                        <CustomLink href="/light" title="Luz" className='w-full text-8xl xs:text-5xl text-center' />
+                        <CustomLink href="/staging" title="Encenação" className='w-full text-8xl xs:text-5xl text-center' />
+                    </div>
                 </div>
 
             </main >
