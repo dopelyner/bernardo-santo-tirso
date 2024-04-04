367b3fb0fdb018ed664b2825b506882f7a5008d8
dopelyner <dalyner@protonmail.com>
2024-04-04 01:09:04 +0000

    feat: added horizontal scroll icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,15 +20,15 @@ export default function Home() {
       <main className='relative w-full min-h-96 flex flex-col justify-center text-dark dark:text-light sm:justify-center sm:items-center'>
         <Layout className='p-0 xl:p-0 lg:p-0 sm:p-0 '>
 
-          <div className='absolute flex right-0 -translate-x-6 p-2'>
+          <div className='absolute sm:flex right-0 -translate-x-6 p-2 hidden'>
             <ArrowHorizontal />
           </div>
 
-          <div className='w-full h-full sm:h-fit absolute z-20 p-32 sm:p-0 bottom-0 bg-transparent/60 dark:bg-transparent/50'>
-            <TitleStickText title="Bernardo Santo Tirso" className='text-light sm:scale-90' />
+          <div className='w-full h-full xs:w-full sm:h-fit absolute z-20 p-32 xl:p-16 sm:p-0 bottom-0 bg-transparent/60 dark:bg-transparent/50'>
+            <TitleStickText title="Bernardo Santo Tirso" className='text-light sm:scale-90 xs:scale-50' />
           </div>
 
-          <div className='w-full flex items-center  justify-center gap-10 overflow-clip sm:overflow-x-scroll ' >
+          <div className='w-full flex items-center justify-center gap-10 overflow-clip sm:overflow-x-scroll ' >
             {homeMedia.map((item, index) => (
               <Image
                 priority
