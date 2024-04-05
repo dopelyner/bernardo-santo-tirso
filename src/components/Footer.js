fa3819a0cb12f83f42dd813d4470869132db3441
dopelyner <dalyner@protonmail.com>
2024-04-05 13:53:37 +0000

    fix: removed unnecessary content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import { socialLinks } from '@/constants'
 import { motion } from 'framer-motion'
-import Image from 'next/image'
 import Link from 'next/link'
 import Layout from './Layout'
 
@@ -8,18 +7,10 @@ const MotionLink = motion(Link)
 
 const Footer = () => {
 
-    const { instagramURL, instagramDeveloper } = socialLinks[0]
+    const { instagramDeveloper } = socialLinks[0]
 
     return (
         <footer className='w-full font-medium text-lg sm:text-base'>
-            {/* <div className='w-full flex justify-center items-center'>
-                <Image
-                    src={"../../images/bernardosantotirso.png"}
-                    alt='Bernardo Santo Tirso'
-                    width={500}
-                    height={500}
-                />
-            </div> */}
             <Layout className='py-10 flex flex-col items-center justify-center text-center lg:flex-col lg:py-10 border-t-2 border-solid
              border-dark dark:border-light dark:text-light'>
                 <span>{new Date().getFullYear()} Bernardo Santo Tirso &copy; Todos os direitos reservados.</span>
