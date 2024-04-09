5f6a8414195da4b946fa114640d80ba1a1cdc337
dopelyner <dalyner@protonmail.com>
2024-04-09 21:35:51 +0000

    feat: added spotlights to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
-import { socialLinks } from '@/constants'
+import { socialLinks, spotlight } from '@/constants'
 import { motion } from 'framer-motion'
 import Link from 'next/link'
 import Layout from './Layout'
+import Image from 'next/image';
 
 const MotionLink = motion(Link)
 
@@ -10,17 +11,34 @@ const Footer = () => {
     const { instagramDeveloper } = socialLinks[0]
 
     return (
-        <footer className='w-full font-medium text-lg sm:text-base'>
-            <Layout className='py-10 flex flex-col items-center justify-center text-center lg:flex-col lg:py-10 border-t-2 border-solid
+        <footer className='relative w-full font-medium text-lg sm:text-base'>
+            <Layout className='flex flex-row justify-between xl:p-8 lg:p-16 md:justify-between py-10 text-center md:flex-col sm:flex-row lg:py-10 border-t-2 border-solid
              border-dark dark:border-light dark:text-light'>
-                <span>{new Date().getFullYear()} Bernardo Santo Tirso &copy; Todos os direitos reservados.</span>
-                <MotionLink
-                    href={instagramDeveloper}
-                    target={'_blank'}
-                    className='mt-2 hover:-translate-y-0.5'
-                >
-                    <span className='hover:font-bold'>Developed by Tiago Lino</span>
-                </MotionLink>
+                <Image
+                    src={spotlight.image}
+                    alt="Spotlight Projector"
+                    className="w-32 lg:w-16 sm:hidden -rotate-45 h-auto object-fill xl:object-cover"
+                    width={500}
+                    height={500}
+                    priority />
+                <div className='flex flex-col items-center justify-center '>
+                    <span>{new Date().getFullYear()} Bernardo Santo Tirso &copy; Todos os direitos reservados.</span>
+                    <MotionLink
+                        href={instagramDeveloper}
+                        target={'_blank'}
+                        className='mt-2 hover:-translate-y-0.5'
+                    >
+                        <span className='hover:font-bold'>Developed by Tiago Lino</span>
+                    </MotionLink>
+                </div>
+
+                <Image
+                    src={spotlight.image}
+                    alt="Spotlight Projector"
+                    className="w-32 lg:w-16 sm:hidden scale-x-[-1] rotate-45 h-auto object-fill xl:object-cover "
+                    width={50}
+                    height={50}
+                    priority />
             </Layout>
         </footer>
     )
