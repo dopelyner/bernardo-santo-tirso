c404cb0f555b98b1e40e37a5fb53fe4960331feb
dopelyner <dalyner@protonmail.com>
2024-04-10 00:43:53 +0000

    fix: divided footer in two sections

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,19 @@ const Footer = () => {
             <Layout className='flex flex-row justify-between sm:justify-center xl:p-8 lg:p-16 md:justify-between py-10 text-center md:flex-col sm:flex-row lg:py-10 border-t-2 border-solid
              border-dark dark:border-light dark:text-light'>
 
-                <div className='flex flex-col items-center justify-center sm:hidden'>
+
+                <div className='flex flex-col items-start justify-center'>
+                    <span>{new Date().getFullYear()} Bernardo Santo Tirso &copy; Todos os direitos reservados.</span>
+                    <MotionLink
+                        href={socialLinks.instagramDeveloper}
+                        target={'_blank'}
+                        className='mt-2 hover:-translate-y-0.5'
+                    >
+                        <span className='hover:font-bold'>Developed by Tiago Lino</span>
+                    </MotionLink>
+                </div>
+
+                <div className='flex flex-wrap gap-10 items-center justify-center sm:hidden'>
                     <MotionLink
                         href={socialLinks.instagramURL}
                         target={'_blank'}
@@ -36,20 +48,7 @@ const Footer = () => {
                             <span className='hover:font-bold'>Facebook</span>
                         </div>
                     </MotionLink>
-                </div>
 
-                <div className='flex flex-col items-center justify-center '>
-                    <span>{new Date().getFullYear()} Bernardo Santo Tirso &copy; Todos os direitos reservados.</span>
-                    <MotionLink
-                        href={socialLinks.instagramDeveloper}
-                        target={'_blank'}
-                        className='mt-2 hover:-translate-y-0.5'
-                    >
-                        <span className='hover:font-bold'>Developed by Tiago Lino</span>
-                    </MotionLink>
-                </div>
-
-                <div className='flex flex-col items-center justify-center sm:hidden'>
                     <MotionLink
                         href={socialLinks.linkedinURL}
                         target={'_blank'}
@@ -60,18 +59,9 @@ const Footer = () => {
                             <span className='hover:font-bold'>Linkedin</span>
                         </div>
                     </MotionLink>
-
-                    <MotionLink
-                        href={socialLinks.facebookURL}
-                        target={'_blank'}
-                        className='mt-2 hover:-translate-y-0.5'
-                    >
-                        <div className='flex flex-row-reverse items-center gap-1'>
-                            <LinkArrow className="!w-5" />
-                            <span className='hover:font-bold'>Instagram</span>
-                        </div>
-                    </MotionLink>
                 </div>
+
+
             </Layout>
         </footer>
     )
