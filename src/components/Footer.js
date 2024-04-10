fbde9dfb5c2db7fffda469ac1e04e249cfda668a
dopelyner <dalyner@protonmail.com>
2024-04-10 00:20:26 +0000

    fix: social media links in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,28 +3,45 @@ import { motion } from 'framer-motion'
 import Link from 'next/link'
 import Layout from './Layout'
 import Image from 'next/image';
+import { ArrowRight, LinkArrow } from './Icons';
 
 const MotionLink = motion(Link)
 
 const Footer = () => {
 
-    const { instagramDeveloper } = socialLinks[0]
-
     return (
         <footer className='relative w-full font-medium text-lg sm:text-base'>
             <Layout className='flex flex-row justify-between sm:justify-center xl:p-8 lg:p-16 md:justify-between py-10 text-center md:flex-col sm:flex-row lg:py-10 border-t-2 border-solid
              border-dark dark:border-light dark:text-light'>
-                <Image
-                    src={spotlight.image}
-                    alt="Spotlight Projector"
-                    className="w-32 lg:w-16 sm:hidden -rotate-45 h-auto object-fill xl:object-cover"
-                    width={500}
-                    height={500}
-                    priority />
+
+                <div className='flex flex-col items-center justify-center sm:hidden'>
+                    <MotionLink
+                        href={socialLinks.instagramURL}
+                        target={'_blank'}
+                        className='mt-2 hover:-translate-y-0.5'
+                    >
+                        <div className='flex flex-row-reverse items-center gap-1'>
+                            <LinkArrow className="!w-5" />
+                            <span className='hover:font-bold'>Instagram</span>
+                        </div>
+                    </MotionLink>
+
+                    <MotionLink
+                        href={socialLinks.facebookURL}
+                        target={'_blank'}
+                        className='mt-2 hover:-translate-y-0.5'
+                    >
+                        <div className='flex flex-row-reverse items-center gap-1'>
+                            <LinkArrow className="!w-5" />
+                            <span className='hover:font-bold'>Facebook</span>
+                        </div>
+                    </MotionLink>
+                </div>
+
                 <div className='flex flex-col items-center justify-center '>
                     <span>{new Date().getFullYear()} Bernardo Santo Tirso &copy; Todos os direitos reservados.</span>
                     <MotionLink
-                        href={instagramDeveloper}
+                        href={socialLinks.instagramDeveloper}
                         target={'_blank'}
                         className='mt-2 hover:-translate-y-0.5'
                     >
@@ -32,16 +49,64 @@ const Footer = () => {
                     </MotionLink>
                 </div>
 
-                <Image
-                    src={spotlight.image}
-                    alt="Spotlight Projector"
-                    className="w-32 lg:w-16 sm:hidden scale-x-[-1] rotate-45 h-auto object-fill xl:object-cover "
-                    width={50}
-                    height={50}
-                    priority />
+                <div className='flex flex-col items-center justify-center sm:hidden'>
+                    <MotionLink
+                        href={socialLinks.linkedinURL}
+                        target={'_blank'}
+                        className='mt-2 hover:-translate-y-0.5'
+                    >
+                        <div className='flex flex-row-reverse items-center gap-1'>
+                            <LinkArrow className="!w-5" />
+                            <span className='hover:font-bold'>Linkedin</span>
+                        </div>
+                    </MotionLink>
+
+                    <MotionLink
+                        href={socialLinks.facebookURL}
+                        target={'_blank'}
+                        className='mt-2 hover:-translate-y-0.5'
+                    >
+                        <div className='flex flex-row-reverse items-center gap-1'>
+                            <LinkArrow className="!w-5" />
+                            <span className='hover:font-bold'>Instagram</span>
+                        </div>
+                    </MotionLink>
+                </div>
             </Layout>
         </footer>
     )
 }
 
 export default Footer
+
+
+{/* <footer className='relative w-full font-medium text-lg sm:text-base'>
+<Layout className='flex flex-row justify-between sm:justify-center xl:p-8 lg:p-16 md:justify-between py-10 text-center md:flex-col sm:flex-row lg:py-10 border-t-2 border-solid
+ border-dark dark:border-light dark:text-light'>
+    <Image
+        src={spotlight.image}
+        alt="Spotlight Projector"
+        className="w-32 lg:w-16 sm:hidden -rotate-45 h-auto object-fill xl:object-cover"
+        width={500}
+        height={500}
+        priority />
+    <div className='flex flex-col items-center justify-center '>
+        <span>{new Date().getFullYear()} Bernardo Santo Tirso &copy; Todos os direitos reservados.</span>
+        <MotionLink
+            href={instagramDeveloper}
+            target={'_blank'}
+            className='mt-2 hover:-translate-y-0.5'
+        >
+            <span className='hover:font-bold'>Developed by Tiago Lino</span>
+        </MotionLink>
+    </div>
+
+    <Image
+        src={spotlight.image}
+        alt="Spotlight Projector"
+        className="w-32 lg:w-16 sm:hidden scale-x-[-1] rotate-45 h-auto object-fill xl:object-cover "
+        width={50}
+        height={50}
+        priority />
+</Layout>
+</footer> */}
