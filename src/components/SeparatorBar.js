8b64b7ab7e84bb35c01aabc07f49256f8a741bb5
dopelyner <dalyner@protonmail.com>
2024-04-12 12:31:16 +0000

    fix: added className

--- src/components/SeparatorBar.js
+++ src/components/SeparatorBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const SeparatorBar = ({ size }) => (
-    <div className="flex justify-center items-center mb-20">
+const SeparatorBar = ({ size, className }) => (
+    <div className={`flex justify-center items-center mb-20 ${className}`}>
         <div className={` ${size === "big"
             ? "w-[800px] h-[3px] xs:w-[260px] bg-dark dark:bg-light m-0 sm:bg-dark sm:dark:bg-light"
             : "w-[200px] h-[3px] xs:w-[150px] bg-dark dark:bg-light m-0 sm:bg-dark sm:dark:bg-light"
