4e93ddf1b1c29e1e4939cb495b3828a2bfaa433b
dopelyner <dalyner@protonmail.com>
2024-05-03 10:40:45 +0000

    fix: changed project to dynamic content

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,10 @@
 import AnimatedQuotes from '@/components/AnimatedQuotes'
 import AnimatedText from '@/components/AnimatedText'
+import { ArrowVertical } from '@/components/Icons'
 import Layout from '@/components/Layout'
 import { CustomLink } from '@/components/Navbar'
 import SeparatorBar from '@/components/SeparatorBar'
-import { quotes } from '@/constants'
+import { projectsSubMenu, quotes } from '@/constants'
 import Head from 'next/head'
 import Image from 'next/image'
 
@@ -16,21 +17,32 @@ const Projects = () => {
             </Head>
 
             <main className='w-full flex flex-col items-center min-h-screen justify-center overflow-hidden text-dark dark:text-light'>
-                <Layout className='pt-16'>
+                <Layout className='pt-16 xs:py-0 xs:pb-6'>
 
-                <AnimatedQuotes
+                    <AnimatedQuotes
                         lines={[quotes.quote2.line1, quotes.quote2.line2, quotes.quote2.line3]}
                         author={quotes.author + " 2/3"}
                     />
 
-                    <SeparatorBar size="big" />
+                    <div className='hidden sm:flex sm:justify-end'>
+                        <ArrowVertical />
+                    </div>
+
+                    <SeparatorBar size="big" className="pt-16" />
 
                     <div className='flex flex-col xl:mb-12 xs:mb-16 items-center justify-center lg:mt-0'>
+
                         <div className='flex flex-col mt-24 gap-y-12'>
-                            <CustomLink href="/cinema" title="Cinema" className='w-full text-8xl xs:text-5xl text-center' />
-                            <CustomLink href="/theater" title="Teatro" className='w-full text-8xl xs:text-5xl text-center' />
-                            <CustomLink href="/light" title="Luz" className='w-full text-8xl xs:text-5xl text-center' />
-                            <CustomLink href="/staging" title="Encenação" className='w-full text-8xl xs:text-5xl text-center' />
+                            {projectsSubMenu.map((item, index) => (
+                                <>
+                                    <CustomLink
+                                        index={index}
+                                        href={item.href}
+                                        title={item.title}
+                                        className='w-full text-8xl xs:text-5xl text-center'
+                                    />
+                                </>
+                            ))}
                         </div>
                     </div>
                 </Layout>
