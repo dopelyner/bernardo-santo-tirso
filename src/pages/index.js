3997e6aa93744d83096b8b396a52fcb7e4991116
dopelyner <dalyner@protonmail.com>
2024-05-07 16:49:48 +0000

    fix: removed unnecessary content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
-import AnimatedText from '@/components/AnimatedText'
-import { ArrowHorizontal, LinkArrow } from '@/components/Icons'
+import { ArrowHorizontal } from '@/components/Icons'
 import Layout from '@/components/Layout'
 import TitleStickText from '@/components/TitleStickText'
 import TransitionEffect from '@/components/TransitionEffect'
-import { homeMedia, projectsSubMenu } from '@/constants'
+import { homeMedia } from '@/constants'
 import Head from 'next/head'
 import Image from 'next/image'
-import Link from 'next/link'
 
 export default function Home() {
 
@@ -39,35 +37,12 @@ export default function Home() {
                 width={500}
                 height={500}
                 title='Bernardo Santo Tirso'
-                alt='Bernardo Santo Tirso'
+                alt='Bernardo Santo Tirso w-full h-full'
               />
             ))
             }
           </div>
         </Layout>
-
-        {/* <Layout className='h-fit'>
-          <div className='flex flex-col items-center justify-center'>
-            <AnimatedText
-              className='text-center sm:text-7xl'
-              text="experiências / vivências" />
-            <p className='text-2xl pt-8'>Desde &apos;95 a produzir momentos... </p>
-
-            <div
-              className='flex flex-row items-center justify-center xl:flex-wrap md:flex-wrap gap-10 pt-16'>
-              {projectsSubMenu.map((projects, index) => (
-                <>
-                  <div className='relative min-h-40 min-w-72 flex items-center justify-center max-w-96 bg-gradient-to-tr via-dark/60 to-80% from-dirt to-oliveDark/60 shadow-primary shadow-md text-light hover:-translate-y-1 hover:cursor-pointer'>
-                    <Link key={index} href={projects.href} className="h-full">
-                      <LinkArrow className="!w-8 absolute top-0 right-0 m-2" />
-                      <h2 className='text-center text-7xl'>{projects.title}</h2>
-                    </Link>
-                  </div></>
-              ))}
-
-            </div>
-          </div>
-        </Layout> */}
 
       </main >
     </>
