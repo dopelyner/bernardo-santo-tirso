ec7c5e82519b650b49c892917870011a4242bbe9
dopelyner <dalyner@protonmail.com>
2024-05-08 14:39:20 +0000

    fix: removed unncessary content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,8 @@
-import { socialLinks, spotlight } from '@/constants'
-import { motion } from 'framer-motion'
-import Link from 'next/link'
-import Layout from './Layout'
-import Image from 'next/image';
-import { ArrowRight, LinkArrow } from './Icons';
+import { socialLinks } from '@/constants';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { LinkArrow } from './Icons';
+import Layout from './Layout';
 
 const MotionLink = motion(Link)
 
@@ -37,7 +36,7 @@ const Footer = () => {
                             <span className='hover:font-bold'>Facebook</span>
                         </div>
                     </MotionLink>
-                    
+
                     <MotionLink
                         href={socialLinks.instagramURL}
                         target={'_blank'}
@@ -68,35 +67,3 @@ const Footer = () => {
 }
 
 export default Footer
-
-
-{/* <footer className='relative w-full font-medium text-lg sm:text-base'>
-<Layout className='flex flex-row justify-between sm:justify-center xl:p-8 lg:p-16 md:justify-between py-10 text-center md:flex-col sm:flex-row lg:py-10 border-t-2 border-solid
- border-dark dark:border-light dark:text-light'>
-    <Image
-        src={spotlight.image}
-        alt="Spotlight Projector"
-        className="w-32 lg:w-16 sm:hidden -rotate-45 h-auto object-fill xl:object-cover"
-        width={500}
-        height={500}
-        priority />
-    <div className='flex flex-col items-center justify-center '>
-        <span>{new Date().getFullYear()} Bernardo Santo Tirso &copy; Todos os direitos reservados.</span>
-        <MotionLink
-            href={instagramDeveloper}
-            target={'_blank'}
-            className='mt-2 hover:-translate-y-0.5'
-        >
-            <span className='hover:font-bold'>Developed by Tiago Lino</span>
-        </MotionLink>
-    </div>
-
-    <Image
-        src={spotlight.image}
-        alt="Spotlight Projector"
-        className="w-32 lg:w-16 sm:hidden scale-x-[-1] rotate-45 h-auto object-fill xl:object-cover "
-        width={50}
-        height={50}
-        priority />
-</Layout>
-</footer> */}
