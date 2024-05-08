6daf127093550bb57f08644dbc4caba632a7eb09
dopelyner <dalyner@protonmail.com>
2024-05-08 11:42:56 +0000

    fix: changed png to avif (home images)

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,10 +9,10 @@ export const spotlight = {
 }
 
 export const homeMedia = [
-    { path: '/images/original/IMG-20240322-WA0004.png', },
-    { path: '/images/original/IMG-20240322-WA0005.png', },
-    { path: '/images/original/IMG-20240322-WA0006.png', },
-    { path: '/images/original/IMG-20240322-WA0007.png', },
+    { path: '/images/original/IMG-20240322-WA0004.avif', },
+    { path: '/images/original/IMG-20240322-WA0005.avif', },
+    { path: '/images/original/IMG-20240322-WA0006.avif', },
+    { path: '/images/original/IMG-20240322-WA0007.avif', },
 ]
 
 export const stagingMedia = {
