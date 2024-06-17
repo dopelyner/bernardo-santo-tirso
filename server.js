4b3e5f1d54787f95c173335cd92603157981a1d5
dopelyner <dalyner@protonmail.com>
2024-06-17 12:29:12 +0000

    fix: server.js updated

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
- 
+
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
-const port = process.env.port || 3000
-// when using middleware `hostname` and `port` must be provided below
+const port = process.env.PORT || 3000
+
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
- 
+
 app.prepare().then(() => {
   createServer(async (req, res) => {
     try {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
       const parsedUrl = parse(req.url, true)
       const { pathname, query } = parsedUrl
- 
+
       if (pathname === '/a') {
         await app.render(req, res, '/a', query)
       } else if (pathname === '/b') {
@@ -29,12 +27,8 @@ app.prepare().then(() => {
       res.statusCode = 500
       res.end('internal server error')
     }
+  }).listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://${hostname}:${port}`)
   })
-    .once('error', (err) => {
-      console.error(err)
-      process.exit(1)
-    })
-    .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`)
-    })
 })
